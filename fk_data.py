03d12dd2939cec75de7920f09f01ccbe52694010
sgupta2501 <sgupta2501@gmail.com>
2021-04-19 00:26:59 +0530

    Add files via upload

--- fk_data.py
+++ fk_data.py
@@ -10,82 +10,42 @@ mydb = mysql.connector.connect(
   database="brawlhalla"
 )
 
-mycursor = mydb.cursor()
+def addFK(dbcon, fkTab, pkTab, fkattr, pkattr):
+  mycursor = dbcon.cursor()
+  
+  fksql="select distinct "+fkattr+ " from "+ fkTab
+  #print(fksql)
+  mycursor.execute(fksql)
+  fkVal=mycursor.fetchall()
 
-mycursor.execute("select distinct clan_id from clan_member")
-fkVal=mycursor.fetchall()
-mycursor.execute("select distinct clan_id from clans")
-pkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table clan_member add foreign key (clan_id) references clans(clan_id)")
-else:
-  print("1 clan_member fk clan_id not added")
-
-mycursor.execute("select distinct clan_id from stats")
-fkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table stats add foreign key (clan_id) references clans(clan_id)")
-else:
-  print("2 stats fk clan_id not added")
-
-
-mycursor.execute("select distinct brawlhalla_id from clan_member")
-fkVal=mycursor.fetchall()
-mycursor.execute("select distinct brawlhalla_id from stats")
-pkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table clan_member add foreign key (brawlhalla_id) references stats(brawlhalla_id)")
-else:
-  print("3 clan_member fk brawlhalla_id not added")
-
-mycursor.execute("select distinct brawlhalla_id from stats_legends")
-fkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table stats_legends add foreign key (brawlhalla_id) references stats(brawlhalla_id)")
-else:
-  print("4 stats_legends fk brawlhalla_id not added")
+  for d in fkVal:
+    if d[0]==None:
+      continue
+    pksql="select "+pkattr+ " from "+ pkTab+ " where "+ pkattr + " = "+ str(d[0])
+    #print(pksql)
+    mycursor.execute(pksql)
+    pkVal=mycursor.fetchall()
+    if len(pkVal) == 0:
+     fksql="delete from "+fkTab+ " where "+ fkattr+ " = "+ str(d[0])
+     mycursor.execute(fksql)
+  
+  s= "alter table "+ fkTab + " add foreign key ("+fkattr+ ") references "+ pkTab+ "("+ pkattr+ ")"
+  print(s)
+  mycursor.execute(s)
+  
 
 
-mycursor.execute("select distinct brawlhalla_id from players_1v1")
-fkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table players_1v1 add foreign key (brawlhalla_id) references stats(brawlhalla_id)")
-else:
-  print("5 players_1v1 fk brawlhalla_id not added")
+
+addFK(mydb, "clan_member", "clans", "clan_id", "clan_id")
+addFK(mydb, "clan_member", "stats", "brawlhalla_id", "brawlhalla_id")
+addFK(mydb, "stats_legends", "stats", "brawlhalla_id", "brawlhalla_id")
+addFK(mydb, "stats_legends", "stats", "brawlhalla_id", "brawlhalla_id")
+addFK(mydb, "players_1v1", "stats", "brawlhalla_id", "brawlhalla_id")
+addFK(mydb, "players_2v2", "stats", "brawlhalla_id_one", "brawlhalla_id")
+addFK(mydb, "players_2v2", "stats", "brawlhalla_id_two", "brawlhalla_id")
+addFK(mydb, "stats_legends", "legends", "legend_id", "legend_id")
+addFK(mydb, "players_1v1", "legends", "best_legend", "legend_id")
+addFK(mydb, "stats", "clans", "clan_id", "clan_id") 
 
 
-mycursor.execute("select distinct brawlhalla_id_one from players_2v2")
-fkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table players_2v2 add foreign key (brawlhalla_id_one) references stats(brawlhalla_id)")
-else:
-  print("6 players_2v2 fk brawlhalla_id_one not added")
-
-
-mycursor.execute("select distinct brawlhalla_id_two from players_2v2")
-fkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table players_2v2 add foreign key (brawlhalla_id_two) references stats(brawlhalla_id)")
-else:
-  print("7 players_2v2 fk brawlhalla_id_two not added")
-
-
-mycursor.execute("select distinct legend_id from stats_legends")
-fkVal=mycursor.fetchall()
-mycursor.execute("select distinct legend_id from legends")
-pkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table stats_legends add foreign key (legend_id) references legends(legend_id)")
-else:
-  print("8 stats_legends fk legend_id not added")
-
-
-mycursor.execute("select distinct best_legend from players_1v1")
-fkVal=mycursor.fetchall()
-if (fkVal==pkVal):
-  mycursor.execute("alter table players_1v1 add foreign key (best_legend) references legends(legend_id)")
-else:
-  print("9 players_1v1 fk legend_id not added")
-
-
-#mydb.commit()
+mydb.commit()
